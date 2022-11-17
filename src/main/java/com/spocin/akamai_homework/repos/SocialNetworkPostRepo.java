e08341be12a8df47eb750704ec709f5b96806da5
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-11-17 22:21:28 +0100

    Implemented SocialNetworkPost repo.

---
+++ src/main/java/com/spocin/akamai_homework/repos/SocialNetworkPostRepo.java
@@ -0,0 +1,8 @@
+package com.spocin.akamai_homework.repos;
+
+import com.spocin.akamai_homework.models.SocialNetworkPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialNetworkPostRepo extends JpaRepository<SocialNetworkPost, Long> {
+
+}
