41122c591b198608a8540074a7ba6be3359314c9
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-11-17 20:20:17 +0100

    Initial commit

---
+++ src/main/java/com/spocin/akamai_homework/AkamaiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.spocin.akamai_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkamaiHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkamaiHomeworkApplication.class, args);
+    }
+
+}
